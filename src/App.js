2b0c9ba5779384481237bb257f52898fa8168403
jpgotopo <jpgotopo@gmail.com>
2020-12-21 02:40:27 -0500

    43 Primeros Pasos con el State de React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
+
+  //Crear Listado de Productos
+  const [ productos, guardarProductos ] = useState([
+    { id: 1, nombre: 'Camisa React Js', precio: 50 },
+    { id: 2, nombre: 'Camisa Συε Js', precio: 40 },
+    { id: 3, nombre: 'Camisa Νοδε Js', precio: 30 },
+    { id: 4, nombre: 'Camisa Ανγυλαρ Js', precio: 30 },
+  ]);
 
   //Obtener la fecga
   const fecha = new Date().getFullYear();
